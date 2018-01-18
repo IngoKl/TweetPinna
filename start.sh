9e5c2d5375615bc81d12a662db1c3eb3afa927f6
Ingo Kleiber <ikleiber@googlemail.com>
2018-01-18 10:58:29 +0100

    fixed default config name

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 screen -S TweetPinna -X quit
 screen -S TweetPinnaDB -X quit
 pkill -f '.*TweetPinna.*'
-screen -d -m -S TweetPinna bash -c 'python TweetPinna.py cfg/BrooklynTest.cfg'
-screen -d -m -S TweetPinnaDB bash -c 'python TweetPinnaDashboard.py cfg/BrooklynTest.cfg'
+screen -d -m -S TweetPinna bash -c 'python TweetPinna.py cfg/TweetPinnaDefault.cfg'
+screen -d -m -S TweetPinnaDB bash -c 'python TweetPinnaDashboard.py cfg/TweetPinnaDefault.cfg'
