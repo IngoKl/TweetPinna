dfdfed8510fa2dbf0efb958598aefe776a9fc73f
Ingo Kleiber <kleiber@heiedu.uni-heidelberg.de>
2019-01-19 21:30:00 +0100

    changed primary method and added further sanity checks

--- restart.sh
+++ restart.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 old=$(< lastcount.txt)
 new_count=$(curl 127.0.0.1:8080/ajax/get/docs-in-collection 2>&1| awk '/[0-9]{7,10}/' | sed 's/ //g')
-# Alternative version since the short version above seems not work on certain systems
-#new_count=$(curl 127.0.0.1:8080/ajax/get/docs-in-collection 2>&1 | awk '{print $1}' | awk '/[0-9]/' | sed 's/ //g')
+
+if [ "$old" == "" ] && [ "$new_count" == "" ]
+ then
+  old=0
+  new_count=0
+fi
+
 echo $new_count > lastcount.txt
 echo "Old Count: $old"
 echo "New Count: $new_count"
@@ -11,25 +16,24 @@ delta=$(expr $new_count - $old)
 
 if ((delta < 100))
  then
-  echo "There seem to be no new tweets; restarting!"
+  echo "There seem to be no new tweets..."
   . start.sh
 fi
 
 # Restart MongoDB; Requires sudo rights: tweetpinna ALL=NOPASSWD:/usr/sbin/service mongodb *
 service=mongodb
-if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+
+if sudo service mongodb status | grep -q 'failed'
  then
-  echo "$service is running; no restart required!"
- else
-  echo "$service has stopped; restarting!"
   sudo service mongodb start
+ else
+  echo "$service is running (method primary)"
 fi
 
-# Alternative method based on checking the service
-#if sudo service mongodb status | grep -q 'enabled'
+# Alternative method based on ps
+#if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
 # then
-#  echo "$service is running; no restart required!"
+#  echo "$service is running (method alt)"
 # else
-#  echo "$service has stopped; restarting!"
 #  sudo service mongodb start
 #fi
